b73e3b6ec4b99f7173be7851f794ef1ae1b3b2ac
DELL <abongoverah1@gmail.com>
2022-02-20 21:57:20 -0800

    first commit

---
+++ app/src/main/java/com/example/vprevision/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.vprevision
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomNavigationView=findViewById<BottomNavigationView>(R.id.bottom)
+        val navController=findNavController(R.id.nave)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
